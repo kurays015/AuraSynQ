38c19fbaaedd6eb1b04435febc35c4e95a15dadb
Kurays <christnarvarte15@gmail.com>
2026-02-15 15:18:12 +0800

    chore: Populate account association details and simplify the miniapp subtitle.

--- miniapp.config.ts
+++ miniapp.config.ts
@@ -9,15 +9,15 @@ const ROOT_URL =
  * @see {@link https://miniapps.farcaster.xyz/docs/guides/publishing}
  */
 export const miniappConfig = {
-    accountAssociation: { // this will be added in step 5
-    "header": "",
-    "payload": "",
-    "signature": ""
+    "accountAssociation": {
+    "header": "eyJmaWQiOjI2OTk4ODcsInR5cGUiOiJhdXRoIiwia2V5IjoiMHg5MEY0MkQ3OTRGNzI2MjFlMTRiRkUxN0VBMGE0RDVGQzNCNTI3NDFEIn0",
+    "payload": "eyJkb21haW4iOiJhdXJhc3lucS52ZXJjZWwuYXBwIn0",
+    "signature": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEHsZmazSxtdHYHMF_FT7jHSSL9VWTAZScFH-rB4RApH4UwOINOwI8mPC4-71n40VvZ7sQoPeAYW1AALJjWUCBG7GwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
   },
     miniapp: {
         version: "1",
         name: "AuraSynQ",
-        subtitle: "A painting mini-app on base app",
+        subtitle: "A mini-app on base app",
         description: "A minimalist digital painting mini-app on base app",
         screenshotUrls: [`${ROOT_URL}/icon.png`],
         iconUrl: `${ROOT_URL}/icon.png`,
